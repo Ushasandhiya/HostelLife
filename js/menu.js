25623d6ffc1cc823d5bc2bfd0f899bd2d4365034
Ushasandhiya <sandhiya.pcy@gmail.com>
2026-01-05 19:31:57 +0530

    Add weekly mess menu data

---
+++ js/menu.js
@@ -0,0 +1,37 @@
+const weeklyMenu = {
+  Monday: {
+    breakfast: "Idli, Sambar, Chutney",
+    lunch: "Rice, Sambar, Poriyal, Curd",
+    dinner: "Chapati, Vegetable Kurma"
+  },
+  Tuesday: {
+    breakfast: "Pongal, Chutney",
+    lunch: "Rice, Rasam, Beans Poriyal, Curd",
+    dinner: "Dosa, Tomato Chutney"
+  },
+  Wednesday: {
+    breakfast: "Upma, Coconut Chutney",
+    lunch: "Rice, Sambar, Cabbage Poriyal, Curd",
+    dinner: "Chapati, Paneer Curry"
+  },
+  Thursday: {
+    breakfast: "Idiyappam, Coconut Milk",
+    lunch: "Rice, Rasam, Potato Fry, Curd",
+    dinner: "Vegetable Fried Rice"
+  },
+  Friday: {
+    breakfast: "Dosa, Sambar",
+    lunch: "Rice, Sambar, Keerai Poriyal, Curd",
+    dinner: "Chapati, Mixed Veg Curry"
+  },
+  Saturday: {
+    breakfast: "Poori, Masala",
+    lunch: "Rice, Chicken Curry / Veg Curry, Curd",
+    dinner: "Lemon Rice"
+  },
+  Sunday: {
+    breakfast: "Masala Dosa",
+    lunch: "Special Meal (Biryani / Veg Biryani)",
+    dinner: "Chapati, Kurma"
+  }
+};
